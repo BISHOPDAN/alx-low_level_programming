742772231b3e72c0cc1469f2be7c36fa217ee380
BISHOPDAN <juliusdaniel554@gmail.com>
2022-07-01 16:28:45 -0700

    fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,29 +6,39 @@
  */
 int main(void)
 {
-	int i, e;
+	 int i, e, g;
 
-	i = 48;
-	e = 48;
-
+	 i = 48;
+	 e = 48;
+	 g = 48;
 	while (e < 58)
-	{
-		i = 48;
-		while (i < 58)
-		{
-			putchar(e);
-			putchar(i);
-			if (i == 57 && e == 57)
-			{
-				break;
-			}
-			putchar(',');
-			putchar(' ');
-			i++;
-		}
-		e++;
-	}
-	putchar('\n');
+	 {
+		 i = 48;
+		 while (i < 58)
+		 {
+			 g = 48;
+			 while (g < 58)
+			 {
+				 if (e != i && e != g && i != g && e < i && i < g)
+				 {
+					 putchar(e);
+					 putchar(i);
+					 putchar(g);
+					 if (i == 56 && e == 55 && g == 57)
+					 {
+						 break;
+					 }
+					 putchar(',');
+					 putchar(' ');
+				 }
+				 g++;
+			 }
+			 i++;
+		 }
+		 e++;
+	 }
+	 putchar('\n');
 
-	return (0);
+	 return (0);
 }
+
